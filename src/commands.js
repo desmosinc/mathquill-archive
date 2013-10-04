06e646af3d31472c14d0511bbba5c09554b08ee9
Jason Merrill <jason@desmos.com>
2013-10-03 20:10:36 -0700

    Automatic rewrite of .sqrt-prefix to .mq-sqrt-prefix
    No remaining instances of 'sqrt-prefix'.

--- src/commands.js
+++ src/commands.js
@@ -403,7 +403,7 @@ LatexCmds['√'] = P(MathCommand, function(_, _super) {
   _.ctrlSeq = '\\sqrt';
   _.htmlTemplate =
       '<span class="mq-non-leaf">'
-    +   '<span class="mq-scaled sqrt-prefix">&radic;</span>'
+    +   '<span class="mq-scaled mq-sqrt-prefix">&radic;</span>'
     +   '<span class="mq-non-leaf sqrt-stem">&0</span>'
     + '</span>'
   ;
@@ -431,7 +431,7 @@ LatexCmds.nthroot = P(SquareRoot, function(_, _super) {
   _.htmlTemplate =
       '<sup class="mq-nthroot mq-non-leaf">&0</sup>'
     + '<span class="mq-scaled">'
-    +   '<span class="sqrt-prefix mq-scaled">&radic;</span>'
+    +   '<span class="mq-sqrt-prefix mq-scaled">&radic;</span>'
     +   '<span class="sqrt-stem mq-non-leaf">&1</span>'
     + '</span>'
   ;
